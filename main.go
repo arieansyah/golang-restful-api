bdc7fdfc52b7a792ffb49c17924e391b827bbf32
arieansyah <arieansyahp.bun@gmail.com>
2023-08-27 16:33:56 +0700

    + dot env on main

--- main.go
+++ main.go
@@ -9,12 +9,16 @@ import (
 	"arieansyah/golang-restful-api/repository"
 	"arieansyah/golang-restful-api/service"
 	"net/http"
+	"os"
 
 	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	errEnv := godotenv.Load()
+	helper.PanicIfError(errEnv)
 
 	db := app.NewDB()
 	validate := validator.New()
@@ -27,7 +31,7 @@ func main() {
 	router.PanicHandler = exception.ErrorHandler
 
 	server := http.Server{
-		Addr:    "localhost:3000",
+		Addr:    os.Getenv("SERVE_URL") + ":" + os.Getenv("SERVE_PORT"),
 		Handler: middleware.NewAuthMiddleware(router),
 	}
 
